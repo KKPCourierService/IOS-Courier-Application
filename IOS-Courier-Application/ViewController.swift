10931932a2797d360cfe6162e518374a0bab16fc
Игорь Коршунов <kroshigor@mail.ru>
2018-01-14 03:24:22 +0700

    Initial Commit

---
+++ IOS-Courier-Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS-Courier-Application
+//
+//  Created by Игорь Коршунов on 14.01.2018.
+//  Copyright © 2018 Игорь Коршунов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
